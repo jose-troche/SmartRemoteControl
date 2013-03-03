2b32d8b6e309162c0d7987fd7015a6f2ad48bf4c
Jose Troche <jtroche@t-sciences.com>
2013-03-02 23:17:50 -0500

    Enhanced Mac server

--- Arduino/RemoteControlServer/run.sh
+++ Arduino/RemoteControlServer/run.sh
@@ -3,13 +3,35 @@
 
 PORT=4444
 SERIAL=/dev/cu.usbmodemfa131
+stty -f $SERIAL gfmt1:cflag=38b00:iflag=10:lflag=0:oflag=0:discard=f:dsusp=19:eof=4:eol=ff:eol2=ff:erase=7f:intr=3:kill=15:lnext=16:min=0:quit=1c:reprint=12:start=11:status=14:stop=13:susp=1a:time=0:werase=17:ispeed=9600:ospeed=9600
 
-while true ; do
-	request=`(echo -e 'HTTP/1.1 200 OK\n\n'; cat index.html) |
-            nc -l $PORT | grep  "^GET /" | cut -d " " -f 2`
-	command=`expr $request : '^/cmd/\([A-z_]*\)'`
-	if [[ $command ]]; then
-		echo Sending command $command
-		echo  $command > $SERIAL
-	fi
+finally () {
+    echo Exit
+    rm response
+    exit
+}
+
+trap finally SIGINT
+
+echo Listening on port $PORT and sending commands to serial port $SERIAL. Ctrl+C to stop
+mkfifo response
+while true; do    
+    cat response | nc -l $PORT | (
+        read line
+        command=`echo $line | cut -d" " -f2`
+        if [[ $command == '/' ]]; then
+            (echo -e 'HTTP/1.1 200 OK\n'; cat index.html) > response
+            echo Sent index.html
+        else
+            command=`expr $command : '/cmd/\([A-z_]*\)'`
+            if [[ $command ]]; then
+                echo $command> $SERIAL
+                #echo Executed $command > response
+                echo 'HTTP/1.1 204 No Content' > response
+                echo Executed $command
+            else
+                echo "" > response
+            fi
+        fi
+    )
 done
