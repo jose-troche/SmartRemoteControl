20b0e51bb8b9c2a20c22658d41fcf4e6ed158472
Jose Troche <jtroche@t-sciences.com>
2013-05-01 13:23:59 -0400

    Updated serial port and added comment on init serial port via IDE launch

--- Arduino/RemoteControlServer/run.sh
+++ Arduino/RemoteControlServer/run.sh
@@ -2,8 +2,9 @@
 # Modify with values in your environment
 
 PORT=4444
-SERIAL=/dev/cu.usbmodemfa131
-stty -f $SERIAL gfmt1:cflag=38b00:iflag=10:lflag=0:oflag=0:discard=f:dsusp=19:eof=4:eol=ff:eol2=ff:erase=7f:intr=3:kill=15:lnext=16:min=0:quit=1c:reprint=12:start=11:status=14:stop=13:susp=1a:time=0:werase=17:ispeed=9600:ospeed=9600
+SERIAL=/dev/cu.usbmodemfd121
+#stty -f $SERIAL gfmt1:cflag=38b00:iflag=10:lflag=0:oflag=0:discard=f:dsusp=19:eof=4:eol=ff:eol2=ff:erase=7f:intr=3:kill=15:lnext=16:min=0:quit=1c:reprint=12:start=11:status=14:stop=13:susp=1a:time=0:werase=17:ispeed=9600:ospeed=9600
+echo Run Arduino IDE to initialize serial port!
 
 finally () {
     echo Exit
