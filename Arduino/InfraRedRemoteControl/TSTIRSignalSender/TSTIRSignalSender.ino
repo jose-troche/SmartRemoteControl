286eef8ced5243a3ca7b95a2c82ed5f0fb9fe749
Jose Troche <jtroche@t-sciences.com>
2013-05-03 07:15:40 -0400

    Got rid of len and now use 0 terminated arrays. Adjusted PULSE_IR_DELAY from 10 to 9

--- Arduino/InfraRedRemoteControl/TSTIRSignalSender/TSTIRSignalSender.ino
+++ Arduino/InfraRedRemoteControl/TSTIRSignalSender/TSTIRSignalSender.ino
@@ -1,8 +1,10 @@
 #define CMD_MAX_LEN 32
+#define MAX_SIGNAL_LEN 200
+#define PULSE_IR_DELAY 9
 #define IRledPin 3 // IR LED connected to digital pin 3
 #define ledPin  13 // Regular LED to pin 13
-#define NELEMS(x)  (sizeof(x) / sizeof(x[0]))
 
+/* Terminate the signal int arrays with 0 or a negative number ! */
 int tv_vol_up[] = {
 456, 428,
 70, 150,
@@ -262,148 +264,6 @@ int tv_power[] = {
 68, 152,
 70, 0};
 
-/*
-int tv_vol_up2[] = {
-454, 428,
-68, 154,
-66, 154,
-68, 154,
-66, 42,
-68, 42,
-66, 42,
-66, 44,
-66, 42,
-66, 156,
-66, 154,
-66, 154,
-68, 42,
-66, 42,
-66, 44,
-66, 42,
-66, 44,
-66, 154,
-66, 154,
-68, 154,
-66, 42,
-68, 42,
-66, 42,
-68, 42,
-66, 42,
-66, 44,
-66, 42,
-66, 44,
-66, 154,
-68, 152,
-68, 154,
-66, 154,
-68, 154,
-66, 4604,
-454, 428,
-70, 152,
-68, 152,
-70, 152,
-68, 40,
-70, 40,
-68, 40,
-70, 38,
-70, 40,
-70, 150,
-70, 152,
-70, 150,
-70, 40,
-70, 38,
-70, 38,
-72, 38,
-70, 38,
-70, 152,
-70, 150,
-70, 152,
-70, 38,
-70, 40,
-70, 38,
-70, 38,
-72, 38,
-70, 38,
-70, 40,
-70, 38,
-70, 152,
-70, 150,
-70, 152,
-70, 150,
-70, 152,
-70, 0};
-
-int tv_vol_down2[] = {
-456, 428,
-68, 152,
-68, 154,
-68, 152,
-70, 40,
-66, 42,
-76, 36,
-64, 42,
-76, 36,
-64, 156,
-68, 154,
-68, 152,
-64, 42,
-68, 42,
-74, 38,
-64, 42,
-72, 38,
-66, 156,
-72, 146,
-70, 40,
-66, 154,
-66, 44,
-70, 40,
-70, 36,
-68, 44,
-64, 44,
-68, 40,
-72, 148,
-68, 42,
-74, 148,
-64, 158,
-74, 146,
-64, 158,
-72, 4594,
-454, 432,
-72, 150,
-64, 156,
-66, 154,
-66, 42,
-66, 42,
-76, 36,
-62, 44,
-68, 44,
-72, 148,
-64, 154,
-76, 148,
-64, 44,
-64, 44,
-74, 36,
-64, 42,
-68, 42,
-70, 150,
-68, 154,
-76, 34,
-64, 156,
-74, 36,
-64, 42,
-68, 44,
-70, 38,
-66, 44,
-70, 38,
-66, 156,
-72, 34,
-66, 156,
-68, 152,
-66, 156,
-68, 152,
-68, 0};
-*/
-
 int heli_up[] = {
 56, 22,
 168, 24,
@@ -470,36 +330,29 @@ void execute(char * command){
   Serial.print("Executing command ");
   Serial.println(command);
   if (strcmp("tv_vol_up", command) == 0){
-    int len = NELEMS(tv_vol_up);
-    sendIRsignal(tv_vol_up, len);
+    sendIRsignal(tv_vol_up);
   }
   else if (strcmp("tv_vol_down", command) == 0){
-    int len = NELEMS(tv_vol_down);
-    sendIRsignal(tv_vol_down, len);
+    sendIRsignal(tv_vol_down);
   }
   else if (strcmp("tv_power", command) == 0){
-    int len = NELEMS(tv_power);
-    sendIRsignal(tv_power, len);
+    sendIRsignal(tv_power);
   }
   else if (strcmp("tv_source", command) == 0){
-    int len = NELEMS(tv_source);
-    sendIRsignal(tv_source, len);
+    sendIRsignal(tv_source);
   }
   else if (strcmp("heli_up", command) == 0){
-    int len = NELEMS(heli_up);
-    sendIRsignal(heli_up, len);
+    sendIRsignal(heli_up);
   }
   else {
     Serial.println("Unknonwn command");
   }
 }
 
-void sendIRsignal(int signal[], int len){
+void sendIRsignal(int signal[]){
   long msecs;
-  Serial.print("len ");
-  Serial.println(len);
   digitalWrite(ledPin, HIGH);
-  for (int i=0; i<len; i++){
+  for (int i=0; signal[i]>0 && i<MAX_SIGNAL_LEN; i++){
     msecs = signal[i]*10;
     if (i%2 == 0){ 
       pulseIR(msecs);
@@ -520,9 +373,9 @@ void pulseIR(long microsecs) {
   while (microsecs > 0) {
     // 38 kHz is about 13 microseconds high and 13 microseconds low
    digitalWrite(IRledPin, HIGH);  // this takes about 3 microseconds to happen
-   delayMicroseconds(10);         // hang out for 10 microseconds, you can also change this to 9 if its not working
+   delayMicroseconds(PULSE_IR_DELAY);  // hang out for 10 microseconds, you can also change this to 9 if its not working
    digitalWrite(IRledPin, LOW);   // this also takes about 3 microseconds
-   delayMicroseconds(10);         // hang out for 10 microseconds, you can also change this to 9 if its not working
+   delayMicroseconds(PULSE_IR_DELAY); // hang out for 10 microseconds, you can also change this to 9 if its not working
  
    // so 26 microseconds altogether
    microsecs -= 26;
