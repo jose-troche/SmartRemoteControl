9456017a47111390405eed9f945d74b12a400a10
Jose Troche <jatroche@gmail.com>
2013-05-03 09:57:35 -0400

    Simplified script

--- CommandSenders/EmotivCommands.py
+++ CommandSenders/EmotivCommands.py
@@ -4,24 +4,23 @@
 import sys, requests
 
 # Url where commands are sent
-ARDUINO_SERVER_URL = 'http://localhost:4444/cmd/'
+# ARDUINO_SERVER_URL = 'http://192.168.11.4:4444/cmd/'
+ARDUINO_SERVER_URL = 'http://10.1.102.70:4444/cmd/'
 
 def send_command(cmd):
     "Sends a command to Arduino Web Server"
     print "Sending command ", cmd
-    #requests.get(ARDUINO_SERVER_URL + cmd)
+    requests.get(ARDUINO_SERVER_URL + cmd)
 
-
-if __name__ == "__main__":
-    # Keep running until q or Ctrl+C
-    print "Receiving commands... (q to quit)"
-    while True:
-        c = sys.stdin.readline().strip().lower()
-        if c == 'q':
-            break
-        elif c == 'u':
-            send_command('tv_vol_up')
-        elif c == 'd':
-            send_command('tv_vol_down')
-        elif c == 'h':
-            send_command('tv_tv')
+# Keep running until q or Ctrl+C
+print "Receiving commands... (q to quit)"
+while True:
+    c = sys.stdin.readline().strip().lower()
+    if c == 'q':
+        break
+    elif c == 'u':
+        send_command('tv_vol_up')
+    elif c == 'd':
+        send_command('tv_vol_down')
+    elif c == 'h':
+        send_command('heli_up')
